e6607b2be2dbf71e5d5d7083026408b1a3287f24
Montana Flynn <montana@montanaflynn.me>
2015-12-28 19:48:09 -0800

    Reorder stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,26 @@
+# Remove fish welcome message
+set -e fish_greeting
+
 # Path to Oh My Fish install.
-source ~/.config/fish/prompt.fish
 set -gx OMF_PATH /Users/montanaflynn/.local/share/omf
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/montanaflynn/.config/omf
-
-# Golang path setting
+# Set GOPATH
 set -gx GOPATH ~/Development/go/
+
+# Golang bin in path
+set --universal fish_user_paths $fish_user_paths $GOPATH/bin/
 
 # Set z path
 set -g Z_SCRIPT_PATH /usr/local/Cellar/z/1.9/etc/profile.d/z.sh
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+# Nice powerline prompt
+source ~/.config/fish/prompt.fish
 
 # Helpers
 source ~/.config/fish/aliases.fish
 
 # Add completeion for git alias
 make_completion g 'git'
-
-# Remove fish welcome message
-set -e fish_greeting
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
